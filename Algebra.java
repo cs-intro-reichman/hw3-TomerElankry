6ab89c96334da7f3b81e9203b3712dc9359d6936
Tomer Elankry <tomerelankry@Tomers-MacBook-Pro.local>
2024-11-22 19:27:53 +0200

    Completed

--- Algebra.java
+++ Algebra.java
@@ -6,83 +6,147 @@
 public class Algebra {
 	public static void main(String args[]) {
 	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3 = 5
-	    System.out.println(minus(7,2));  // 7 - 2 = 5 
-   		System.out.println(minus(2,7));  // 2 - 7 = -5
- 		System.out.println(times(3,4));  // 3 * 4 = 12
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2 = 10
-   		System.out.println(pow(5,3));      // 5^3 = 125
-   		System.out.println(pow(3,5));      // 3^5 = 243
-   		System.out.println(div(12,3));   // 12 / 3 = 4 
-   		System.out.println(div(5,5));    // 5 / 5  = 1
-   		System.out.println(div(25,7));   // 25 / 7 = 3
-   		System.out.println(mod(25,7));   // 25 % 7 = 4
-   		System.out.println(mod(120,6));  // 120 % 6 = 0  
+	    //System.out.println(plus(2,3));   // 2 + 3 = 5
+	    //System.out.println(minus(-3,-3));  // 7 - 2 = 5 
+   		//System.out.println(minus(2,7));  // 2 - 7 = -5
+ 		//System.out.println(times(3,-1));  // 3 * 4 = 12
+   		//System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2 = 10
+   		//System.out.println(pow(5,3));      // 5^3 = 125
+   		//System.out.println(pow(-3,3));      // 3^5 = 243
+   		//System.out.println(div(-12,-3));   // 12 / 3 = 4 
+   		//System.out.println(div(5,5));    // 5 / 5  = 1
+   		//System.out.println(div(25,7));   // 25 / 7 = 3
+   		//System.out.println(mod(25,7));   // 25 % 7 = 4
+   		//System.out.println(mod(120,6));  // 120 % 6 = 0  
    		System.out.println(sqrt(36)); // = 6
 		System.out.println(sqrt(263169)); 
-   		System.out.println(sqrt(76123));
+   		System.out.println(sqrt(15));
 	}  
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		for(int i=0;i<x2;i++)
-	       {
-			  x1++;
-	       }
+		if(x2>0)
+		{
+			for(int i=0;i<x2;i++)
+			{
+			   x1++;
+			}
+		}
+		if(x2<0)
+		{
+			for(int i=0;i>x2;i--)
+			{
+			   x1--;
+			}
+		}
+		
 		   return x1;
 		
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		for(int i =x2;i>0;i--){
-			x1--;
+		if(x2>0)
+		{
+			for(int i =x2;i>0;i--){
+				x1--;
+			}
 		}
+		if(x2<0)
+		{
+			for(int i =x2;i<0;i++){
+				x1++;
+			}
+		}
+		
 		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		int a = x1;
-		for(int i=0;i<minus(x2,1);i++)
+		int per = x1;
+		if(x1==0 || x2==0) return 0;
+		if(x1>0 && x2>0 || x1<0 && x2>0)
 		{
-			x1 = plus(x1,a);
+			for(int i=1;i<x2;i++) x1 = plus(x1,per);
+			return x1;
 		}
-		return x1;
+		if(x2<0 && x1<0)
+		{
+			for(int j = 0;j>minus(x2, 1);j--) x1 = minus(x1, per);
+			return x1;
+		}
+		if(x1>0 && x2<0)
+		{
+		    for(int i=1;i>x2;i--) x1 = minus(x1,per);
+			return x1;	
+		}
+		return 0;
+
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		int a= x;
-		for(int i=0;i<minus(n, 1);i++)
+		int a = 1;
+		for(int i = 0;i<n;i++)
 		{
-			x=times(x, a);
+			a = times(x, a);
 		}
-		return x;
-		
+		return a;
+
 	}
 
 	// Returns the integer part of x1 / x2 
-	public static int div(int x1, int x2) {
-		int i=0;
-		while (x1>=x2)
+	public static int div(int x1, int x2) 
+	{
+		int i = 0;
+		if(x1==0||x2==0) return 0;
+		if(x1>0 && x2>0)
 		{
-			x1=minus(x1, x2);
-			i++;
+			while(x1>=x2) 
+			{
+				x1=minus(x1, x2);
+				i++;
+			}
+			return i;
 		}
-		return i;
+		if(x1<0 && x2>0)
+		{
+			while (x1<0)
+		    {
+				x1 = plus(x1, x2);
+				i++;			
+			}
+			return times(i, -1);
+		}
+		if(x1<0 && x2<0)
+		{
+			if(x1>x2)return 0;
+			if (x1==x2) return 1;
+			else while (x1<0) 
+			{
+				x1 = minus(x1, x2);
+				i++;		
+			} 
+			return i;
+		}
+		return 0;
 	}
+
+	
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) { return x1-div(x1,x2)*x2; }	
 
-	// Returns the integer part of sqrt(x) by the Bi - section method
+	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) 
 	{
+		if(x==0) return 0;
 		int i =1;
 		while (pow(i,2)<x)
 		{
 			i++;
+			if(pow(i+1,2)>x) break;
 		}
 		return i;
 	}	  	  
--- Anagram.java
+++ Anagram.java
@@ -2,34 +2,60 @@
 public class Anagram {
 	public static void main(String args[]) {
 		// Tests the isAnagram function.
-		System.out.println(isAnagram("silent","listen"));  // true
+		/*System.out.println(isAnagram("silent","listen"));  // true
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
-		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
+		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lords Voldemort")); // true
 
 		// Tests the preProcess function.
 		System.out.println(preProcess("What? No way!!!"));
+		System.out.println(preProcess("William Shakespeare"));
+		System.out.println(preProcess("I am a weakish speller"));*/
 		
-		// Tests the randomAnagram function.
-		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
+		 //Tests the randomAnagram function.
+		System.out.println("silentre and " + randomAnagram("silentre") + " are anagrams.");
 		
-		// Performs a stress test of randomAnagram 
-		String str = "1234567";
-		Boolean pass = true;
-		//// 10 can be changed to much larger values, like 1000
-		for (int i = 0; i < 10; i++) {
-			String randomAnagram = randomAnagram(str);
-			System.out.println(randomAnagram);
-			pass = pass && isAnagram(str, randomAnagram);
-			if (!pass) break;
-		}
-		System.out.println(pass ? "test passed" : "test Failed");
+		//Performs a stress test of randomAnagram 
+		//String str = "1234567";
+	    //Boolean pass = true;
+		 //10 can be changed to much larger values, like 1000
+		//for (int i = 0; i < 10; i++) {
+		//String randomAnagram = randomAnagram(str);
+		//	System.out.println(randomAnagram);
+		//	pass = pass && isAnagram(str, randomAnagram);
+		//	if (!pass) break;
+		
+
+		//String sr = "12345";
+		
 	}  
 
 	// Returns true if the two given strings are anagrams, false otherwise.
-	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+	public static boolean isAnagram(String str1, String str2)
+	{
+		String s1 = preProcess(str1);
+		String s2 = preProcess(str2);
+		int counter1=0;
+		int counter2=0;
+		for(int i=0;i<s1.length();i++)
+		{
+			if(s1.charAt(i)!=32)
+			{
+				for(int j=0;j<s1.length();j++)
+			        {
+				        if(s1.charAt(j)==s1.charAt(i)) counter1++;
+			        }
+
+			    for(int s = 0; s<s2.length();s++)
+			        {
+				        if(s2.charAt(s)==s1.charAt(i)) counter2++;
+			        }
+
+			    if(counter1!=counter2) return false;
+			}
+			
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
@@ -37,13 +63,48 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
+		String rep="";
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)>=65 && str.charAt(i)<=90) rep+=(char)(str.charAt(i)+32);
+			if(str.charAt(i)>=97 && str.charAt(i)<=122) rep+=str.charAt(i);
+			if(str.charAt(i)==32)rep+=str.charAt(i);
+			
+		}
+		return rep;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		String indxOfRandom= "";
+		String newIndx="";
+		int svr=0;
+		String newS="";
+
+		for(int i=0;i<str.length();i++)
+		{
+			indxOfRandom+=i;
+			
+		}
+		while (newIndx.length()<indxOfRandom.length())
+		{
+			svr = (int)(Math.random() * indxOfRandom.length());
+			String c = "" + svr;
+			for(int j = 0;j<indxOfRandom.length();j++)
+			{
+				if(-1==newIndx.indexOf(c)) newIndx+=c;
+			}	
+		}
+
+		for(int s= 0 ;s<newIndx.length();s++) 
+		{
+			int m = newIndx.charAt(s) - '0';
+			newS+=str.charAt(m);
+		}
+		return newS;
 	}
+
+	
+	
 }
